dbfd596ffcaa8581c470fb4c5b2eb24a446bae8e
limeihua0101 <limeihua0101@gmail.com>
2022-12-12 14:33:24 +0900

    텍스트 문구 수정

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React Deployment test
+                    Learn React Deployment test test
                 </a>
             </header>
         </div>
