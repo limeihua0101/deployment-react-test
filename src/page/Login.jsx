d37c139a629ffbcd53555e67ef7868ebb22b7f0c
limeihua0101 <limeihua0101@gmail.com>
2022-12-20 15:11:36 +0900

    change git action mian.yml file add deployment code

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-
 import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
+    const ubuntu_ssh_key = process.env.REACT_APP_UBUNTU_SSH_KEY;
+    console.log(`REACT_APP_UBUNTU_SSH_KEY   === ${ubuntu_ssh_key}`);
 
     const navigate = useNavigate();
     const [userInfo, setUserInfo] = useState({
@@ -11,7 +12,6 @@ export const Login = () => {
     });
 
     const handleInputChange = (target) => {
-        
         const { name, value } = target;
 
         switch (name) {
@@ -34,7 +34,8 @@ export const Login = () => {
     };
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Login Page </h1>
+            <h1>GITHUB ACTION DEPLOYMENT TEST</h1>
             <form action="">
                 <input
                     type="text"
